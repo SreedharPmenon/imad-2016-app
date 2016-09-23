159a84c24856219d8cff83677bf8b27a3dd9627f
SreedharPmenon <sreepmenon@gmail.com>
2016-09-23 23:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,59 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var article_onec={
-   var date;
-   var content
+var article_one={
+   title:'article-one',
+   heading:'sreedhar p menon',
+   date: '25-09-2016',
+   content :`<p>
+            fdbfjdjasbcbjbgjfdkkas;dkd;k;df
+            ffnlfdsjfnlfdsf;lkdl;kfejg;jf;dkd;
+        </p>
+        <p>
+            jdfdfdflajfjdjhfladkas;jdfjhflkkdfl;kd
+            dbfjdjfkldjfdjflksjfklnfkldjf;lkdl;kf
+        </p>
+        <p>
+            sldfdbfjdfdjbfkdbfbdsbfbvdsbfbdkfbdbfkf
+            dhbfdbfjdjkfbdbfkjdfbjfnlkbfkjlnfbkb
+        </p>`
+   
 };
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+
+var htmltemplate=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    </head>
+ <body>
+     <div>
+     <a href="/">
+         HOME
+     </a>  
+     <h3>
+        ${heading}
+     </h3>
+     </div>
+     <div>
+         ${date}
+    </div>
+    <div>
+        ${content}
+        
+    </div>
+</body>
+    
+</html>
+    
+`;
+return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -24,7 +73,7 @@ app.get('/ui/main.js',function(req,res){
    res.sendFile(path.join(__dirname,'ui','main.js')); 
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.send(createTemplate(article_one));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
